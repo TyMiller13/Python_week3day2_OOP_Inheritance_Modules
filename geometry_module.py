85af1db9d4df6a996d8c98eed096ef77464cc459
Tyler Miller <tylermiller1321@outlook.com>
2022-12-13 22:34:49 -0600

    first commit

---
+++ geometry_module.py
@@ -0,0 +1,11 @@
+import math
+
+print("You are in geometry module!")
+
+
+def area_of_circle(radius):
+    return math.pi*(radius**2)
+
+
+def hypotenuse(side1, side2):
+    return math.sqrt(side1**2 + side2**2)
